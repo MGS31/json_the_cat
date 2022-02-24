c7f3765f3ee77d8b5adc01c2fed57ae74406f5b5
Matt <mgstankov30@gmail.com>
2022-02-24 14:10:09 -0500

    add completed code for breedFetcher.js

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,17 @@
 const request = require('request');
+
+let breed = process.argv[2];
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  if (error !== null) {
+    console.error('Error 404: URL is incorrect');
+    process.exit();
+  }
+  if (breed === undefined) {
+    console.error('Error 404: No breed found');
+    process.exit();
+  }
+  const data = JSON.parse(body);
+  let descript = data[0].description;
+  console.log(descript);
+});
